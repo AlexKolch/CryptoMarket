449694438223bbb0d4c2d4fe7c1974f2572fcca6
Alexey Kolch <alexeykolychenkov94@gmail.com>
2024-07-05 10:04:30 +0300

    Initial Commit

---
+++ SwiftUICrypto/SwiftUICryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICryptoApp.swift
+//  SwiftUICrypto
+//
+//  Created by Алексей Колыченков on 05.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
