ed4338f2693e425fc1b785b24fcd0ab0efc7b061
Alexey Kolch <alexeykolychenkov94@gmail.com>
2024-07-10 11:15:41 +0300

    NetworkManager add

--- SwiftUICrypto/Services/CoinDataService.swift
+++ SwiftUICrypto/Services/CoinDataService.swift
@@ -12,7 +12,8 @@ final class CoinDataService {
     
     @Published var allCoins: [Coin] = []
 //    var cancellables = Set<AnyCancellable>()
-    var coinSubscription: AnyCancellable? // здесь подписка на получение монет, чтобы потом было удобно отменить именно ее и не искать во множестве
+    /// здесь подписка на получение монет, чтобы потом было удобно отменить именно ее и не искать во множестве cancellables
+    var coinSubscription: AnyCancellable?
     
     init() {
         getCoins()
@@ -21,25 +22,14 @@ final class CoinDataService {
     private func getCoins() {
         guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=250&sparkline=true&price_change_percentage=24h") else {return}
         
-       coinSubscription = URLSession.shared.dataTaskPublisher(for: url)
-            .subscribe(on: DispatchQueue.global(qos: .default)) //меняет контекст выполнения на бэк поток
-            .tryMap { (result) -> Data in
-                    //проверяем данные
-                guard let response = result.response as? HTTPURLResponse,
-                      response.statusCode >= 200 && response.statusCode < 300 else {
-                    throw URLError(.badServerResponse)
-                }
-                
-                return result.data
-            }
-            .receive(on: DispatchQueue.main) //при получении переходим в main
+        coinSubscription = NetworkManager.downloadData(for: url)
             .decode(type: [Coin].self, decoder: JSONDecoder())
             .sink { completion in
                 switch completion {
                 case .finished:
                     break
                 case .failure(let error):
-                    print("Completion decod finished with error: \(error.localizedDescription)")
+                    print("Completion finished with error: \(error.localizedDescription)")
                 }
             } receiveValue: { [weak self] returnedCoins in
                 self?.allCoins = returnedCoins
---
+++ SwiftUICrypto/Utilities/NetworkManager.swift
@@ -0,0 +1,50 @@
+//
+//  NetworkManager.swift
+//  SwiftUICrypto
+//
+//  Created by Алексей Колыченков on 10.07.2024.
+//
+
+import Foundation
+import Combine
+
+final class NetworkManager {
+    
+    enum NetworkingError: LocalizedError {
+        case badURLResponse(url: URL)
+        case unknown
+        
+        var errorDescription: String? {
+            switch self {
+            case .badURLResponse(url: let url): return "[🔥] Bad response from URL: \(url)"
+            case .unknown: return "[⚠️] Unknown error"
+            }
+        }
+    }
+    
+    
+    static func downloadData(for url: URL) -> AnyPublisher<Data, Error> {
+       return URLSession.shared.dataTaskPublisher(for: url)
+             .subscribe(on: DispatchQueue.global(qos: .default)) //меняет контекст выполнения на бэк поток
+             .tryMap { (result) -> Data in
+                     //проверяем данные
+                 guard let response = result.response as? HTTPURLResponse,
+                       response.statusCode >= 200 && response.statusCode < 300 else {
+                     throw NetworkingError.badURLResponse(url: url)
+                 }
+                 
+                 return result.data
+             }
+             .receive(on: DispatchQueue.main) //при получении переходим в main
+             .eraseToAnyPublisher() //стирает длинный возвращаемый тип и делает его AnyPublisher
+    }
+}
+
+
+
+
+
+
+
+
+
