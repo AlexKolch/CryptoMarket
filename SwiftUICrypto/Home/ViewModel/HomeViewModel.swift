fd9f6d76e61d8444d3123c25ff61ece9fa6df60f
Alexey Kolch <alexeykolychenkov94@gmail.com>
2024-12-10 17:47:13 +0300

    SearchBar logic done

--- SwiftUICrypto/Home/ViewModel/HomeViewModel.swift
+++ SwiftUICrypto/Home/ViewModel/HomeViewModel.swift
@@ -12,6 +12,7 @@ final class HomeViewModel: ObservableObject {
     
     @Published var allCoins: [Coin] = []
     @Published var portfolioCoins: [Coin] = []
+    
     @Published var searchText: String = ""
     
     private let dataService = CoinDataService() //сервис получения монет
@@ -25,13 +26,41 @@ final class HomeViewModel: ObservableObject {
         addSubscibers()
     }
     
-    ///создается подписка на полученные монеты из API
+    ///создается подписка на полученные монеты из API и обновляет локальную allCoins
     private func addSubscibers() {
-        dataService.$allCoins
-        //подписываемся на @Published var allCoins
-            .sink { [weak self] coins in
-                self?.allCoins = coins
+//        dataService.$allCoins
+//        //подписываемся на @Published var allCoins
+//            .sink { [weak self] coins in
+//                self?.allCoins = coins
+//            }
+//            .store(in: &cancellables) //Эта подписка больше не нужна, т к используем combineLatest ниже
+        
+        $searchText
+        //объединяем с подпиской на dataService.$allCoins для использования двух значений searchText и [Coin] для фильтрации
+            .combineLatest(dataService.$allCoins)
+        //сделаем задержку, чтобы функция фильтрации не запускалась сразу же после ввода
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+        //преобразуем вводимый текст в массив результата поиска
+            .map { (inputText, allCoins) -> [Coin] in //после combineLatest мы имеем в параметрах вводимый текст и массив монет
+                //если searchBar пустой, то возвращаем все монеты
+                guard !inputText.isEmpty else {
+                    return allCoins
+                }
+                
+                //идет поиск/фильтрация в searchBar
+                let searchingLowercasedText = inputText.lowercased() //преобразуем в нижний регистр для верной фильтрации
+                let filteredCoins = allCoins.filter { coin in
+                    //проверим есть ли такие названия в имени, символе или id
+                    coin.name.lowercased().contains(searchingLowercasedText) || coin.symbol.lowercased().contains(searchingLowercasedText) || coin.id.lowercased().contains(searchingLowercasedText)
+//                    coin.name.lowercased().contains(lowercasedText)
+                }
+                return filteredCoins
             }
+        //синхронизируем массив allCoins с отфильтрованными монетами
+            .sink { [weak self] returnedCoins in
+                self?.allCoins = returnedCoins
+            }
+        //сохраним Publisher
             .store(in: &cancellables)
     }
 }
